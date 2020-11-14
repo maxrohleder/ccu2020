aa39328cbb3e6abe0607bb1f634a7b4e77346647
maxrohleder <Maxi.rohleder@fau.de>
2020-11-14 17:53:35 +0000

    importing Login module in router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
-import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [  {path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
