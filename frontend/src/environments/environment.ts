2de6a55294fcbb901662b78c11e9a470edeb9b6f
maxrohleder <Maxi.rohleder@fau.de>
2020-11-14 16:04:18 +0000

    add firebase credentials

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPfMgaJJLAL_Gi7Y8EjvzDQ2xcxrcTDJA",
+    authDomain: "covid-travel-buddy.firebaseapp.com",
+    databaseURL: "https://covid-travel-buddy.firebaseio.com",
+    projectId: "covid-travel-buddy",
+    storageBucket: "covid-travel-buddy.appspot.com",
+    messagingSenderId: "747042743357",
+    appId: "1:747042743357:web:de1406c95cfcee72917654"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
