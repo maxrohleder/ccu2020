50a10f4762f7a7bf4925604521fe22889c7168b0
maxrohleder <Maxi.rohleder@fau.de>
2020-11-14 15:55:02 +0000

    add login component on / route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
