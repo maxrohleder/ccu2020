569a7d6c97e345cd261996d8913c4c305498a131
maxrohleder <Maxi.rohleder@fau.de>
2020-11-15 18:45:49 +0000

    add navbar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,8 +12,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
-  declarations: [AppComponent, LoginComponent, ProfileComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    DashboardComponent,
+    NavbarComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,6 +33,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     FirebaseUIModule.forFeature({ tosUrl: 'LOGIN_MODULE' }),
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
