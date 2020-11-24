832d4e3ed4a2e7b75e76fe71513eddabf2f6c7ed
JulianJendryka <julian@jendryka.de>
2020-11-24 10:52:33 +0100

    added generateChecklist clearChecklist and appendChecklist to serviceChecklist

--- frontend/src/app/checklist.service.ts
+++ frontend/src/app/checklist.service.ts
@@ -1,9 +1,87 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChecklistService {
+  constructor() {}
 
-  constructor() { }
+  checklist = [];
+
+  appendCheckList(): any[] {
+    console.log('appendCheckList');
+
+    var item1 = {
+      name: 'Make appointment for Covid-19 Test',
+      time: '72 hours',
+      checked: false,
+    };
+
+    var item2 = {
+      name: 'Conduct Covid-19 Test',
+      time: '72 hours',
+      checked: false,
+    };
+    var item3 = {
+      name: 'Upload Covid-19 Test result',
+      checked: false,
+    };
+    var item4 = {
+      name: 'Receive QR-Code for entry',
+      checked: false,
+    };
+    var ToDo1 = {
+      title: 'Valid Identification Card',
+      text: 'Check that validation is at least 6 weeks',
+      allchecked: false,
+      check: [],
+    };
+
+    ToDo1.check.push(item1);
+    ToDo1.check.push(item2);
+    ToDo1.check.push(item3);
+    ToDo1.check.push(item4);
+
+    this.checklist.push(ToDo1);
+    //console.log(this.checklist);
+
+    //TODO VLT ZUM BACK-END SCHICKN
+
+    return this.checklist;
+  }
+
+  //TODO FROM BACK-END
+  getCheckList(input): any[] {
+    console.log('getCheckList');
+
+    //TODO FROM BACK-END
+    //TODO SOME STUFF WITH input
+    console.log(input);
+    for (var value_index in input) {
+      console.log(input[value_index].value);
+    }
+
+    var item1 = {
+      name: 'Make appointment for Covid-19 Test',
+      time: '72 hours',
+      checked: false,
+    };
+    var ToDo1 = {
+      title: 'Valid Identification Card',
+      text: 'Check that validation is at least 6 weeks',
+      allchecked: false,
+      check: [],
+    };
+
+    ToDo1.check.push(item1);
+    this.checklist.push(ToDo1);
+    //console.log(this.checklist);
+    return this.checklist;
+  }
+
+  clearCheckList(): any[] {
+    this.checklist = [];
+    console.log('Checklist Reset');
+    return this.checklist;
+  }
 }
