6534866c8167a85ab3369e52bc836c0fc96974f9
JulianJendryka <julian@jendryka.de>
2020-11-30 14:06:12 +0100

    added routing to components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NewsComponent } from './news/news.component';
+import { ForumComponent } from './forum/forum.component';
+import { CovidInfoComponent } from './covid-info/covid-info.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'home', component: TravelComponent },
   { path: 'news', component: NewsComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'info', component: CovidInfoComponent },
 ];
 
 @NgModule({
