f668e0d271608fcc93d8e1f96ee3e2acad29627e
maxrohleder <Maxi.rohleder@fau.de>
2020-12-06 19:31:42 +0000

    styling home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToTravel(): void {
+    this.router.navigate(['travel']);
   }
-
+  goToNews(): void {
+    this.router.navigate(['news']);
+  }
+  goToForum(): void {
+    this.router.navigate(['forum']);
+  }
+  goToInfo(): void {
+    this.router.navigate(['info']);
+  }
 }
