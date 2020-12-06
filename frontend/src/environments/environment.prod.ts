4eae4dc22b95ad3ed28ede089d937802e91493ff
maxrohleder <Maxi.rohleder@fau.de>
2020-12-06 20:01:27 +0000

    add production config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDPfMgaJJLAL_Gi7Y8EjvzDQ2xcxrcTDJA',
+    authDomain: 'covid-travel-buddy.firebaseapp.com',
+    databaseURL: 'https://covid-travel-buddy.firebaseio.com',
+    projectId: 'covid-travel-buddy',
+    storageBucket: 'covid-travel-buddy.appspot.com',
+    messagingSenderId: '747042743357',
+    appId: '1:747042743357:web:de1406c95cfcee72917654',
+  },
 };
