12855b60b6403e01bfb2b3cfcd239a56ce2a363a
JulianJendryka <julian@jendryka.de>
2020-12-07 15:37:02 +0100

    news component with basic css

--- frontend/src/app/news/news.component.ts
+++ frontend/src/app/news/news.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.sass']
+  styleUrls: ['./news.component.sass'],
 })
 export class NewsComponent implements OnInit {
+  constructor(private account: AccountService) {}
 
-  constructor() { }
+  news_list = {};
+  selectedCountry = 'Portugal';
+
+  countries = ['Germany', 'France', 'Portugal', 'Spain', 'USA'];
 
   ngOnInit(): void {
+    this.news_list = this.account.getNews();
   }
-
 }
+//<img [src]="news.picture" alt="profil" />
