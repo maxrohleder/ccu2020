259e3b2ab00a3a57e6e1a331df5416e2a992164a
maxrohleder <Maxi.rohleder@fau.de>
2020-12-13 15:52:37 +0100

    implemented data theme

--- backend/src/controllers/DataController.ts
+++ backend/src/controllers/DataController.ts
@@ -1,8 +1,23 @@
+import { MockCountryData } from "../mock-objects/countryData";
+import { MockNewsData } from "../mock-objects/newsData";
+import { MockNotification } from "../mock-objects/notificationData";
 class DataController {
   defaultMethod() {
     return {
       text: `You've reached the ${this.constructor.name} default method`,
     };
+  }
+
+  getAllNews() {
+    return MockNewsData;
+  }
+
+  getAllAlerts() {
+    return MockNotification;
+  }
+
+  getAllCountryInfo() {
+    return MockCountryData;
   }
 }
 
--- backend/src/routers/data/DataRouter.ts
+++ backend/src/routers/data/DataRouter.ts
@@ -17,9 +17,26 @@ class DataRouter {
    * Connect routes to their matching controller endpoints.
    */
   private _configure() {
-    this._router.get("/", (req: Request, res: Response, next: NextFunction) => {
-      res.status(200).json(this._controller.defaultMethod());
-    });
+    this._router.get(
+      "/allCountries",
+      (req: Request, res: Response, next: NextFunction) => {
+        res.status(200).json(this._controller.getAllCountryInfo());
+      }
+    );
+
+    this._router.get(
+      "/news",
+      (req: Request, res: Response, next: NextFunction) => {
+        res.status(200).json(this._controller.getAllNews());
+      }
+    );
+
+    this._router.get(
+      "/alerts",
+      (req: Request, res: Response, next: NextFunction) => {
+        res.status(200).json(this._controller.getAllAlerts());
+      }
+    );
   }
 }
 
